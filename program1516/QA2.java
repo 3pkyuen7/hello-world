9a9977c6953fe5408cf895ebbb6ac596b8769b59
3pkyuen7 <3poonkamyuen7@gmail.com>
2016-12-27 08:25:21 +0800

    Update QA2.java

--- program1516/QA2.java
+++ program1516/QA2.java
@@ -1,21 +1,9 @@
 public class QA2{
 	public static void main(String[]args){
 		int x = 1, y = 2, z = 3;
-		ai(x,y,z);
-		aii(x,y,z);
-		aiii(x,y,z);
-		aiv(x,y,z);
-	}
-	public static void ai(int x, int y, int z){
 		System.out.print("Answer of \"" + x + y + z);
-	}
-	public static void aii(int x, int y, int z){
 		System.out.print("\" should be = " + x * y * z);
-	}
-	public static void aiii(int x, int y, int z){
 		System.out.print("\nx++ - --y + ++z = ");
-	}
-	public static void aiv(int x, int y, int z){
 		System.out.print(x++ - --y + ++z);
 	}
 }
