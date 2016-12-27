ac675e2115c247f15375eca1ba2fe9eddb1fabd4
3pkyuen7 <3poonkamyuen7@gmail.com>
2016-12-27 08:26:22 +0800

    Update QA8.java

--- program1516/QA8.java
+++ program1516/QA8.java
@@ -2,7 +2,7 @@ public class QA8{
 	public static void main(String[] args){
 	int [] data = {3,9,5,7,12};
 	printReverseArray(data);
-}
+	}
 	public static void printReverseArray(int []data){
 		for(int i = data.length -1;i>= 0;i--){
 			System.out.print(data[i]+" ");
